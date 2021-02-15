71975dea0a469449616a657480e33b3bbb778326
Desmond Mpofu <desmondmpofu@gmail.com>
2021-02-15 18:15:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Desmond</h1>
+      <h2>Start editing to see some magic happen!...Here we go</h2>
+    </div>
+  );
+}
